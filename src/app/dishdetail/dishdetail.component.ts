d87a2429ebccaa413b0d11720c6517debbfbc05d
Eric Ngako <ericnk1@live.com>
2020-11-13 15:52:01 +0200

    Animations

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -5,12 +5,26 @@ import {DishService} from '../services/dish.service';
 import {Params, ActivatedRoute} from '@angular/router';
 import {switchMap} from 'rxjs/operators';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -18,6 +32,7 @@ export class DishdetailComponent implements OnInit {
   comment: Comment;
   @ViewChild('cform') commentFormDirective;
 
+  visibility = 'shown';
   dishcopy: Dish;
   errMess: string;
   dishIds: string[];
@@ -52,9 +67,10 @@ export class DishdetailComponent implements OnInit {
 
     this.dishService.getDishIds().subscribe((dishIds) => this.dishIds = dishIds);
     this.route.params
-      .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
-        errmess => this.errMess = <any>errmess );
+      .pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishService.getDish(params['id']);
+      }))
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
+        errmess => this.errMess = <any> errmess );
   }
 
   setPrevNext(dishId: string): void {
