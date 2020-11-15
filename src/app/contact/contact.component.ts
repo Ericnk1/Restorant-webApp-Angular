db76ab2b00107a7cb09432fc92889ad7dc026fdf
Eric Ngako <ericnk1@live.com>
2020-11-15 20:12:37 +0200

    Component Test

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,7 +12,7 @@ import {switchMap} from 'rxjs/operators';
   styleUrls: ['./contact.component.css'],
   host: {
     '[@flyInOut]': 'true',
-    'style': 'display: block;'
+    style: 'display: block;'
   },
   animations: [
     flyInOut(),
@@ -33,30 +33,30 @@ export class ContactComponent implements OnInit {
   hideForm = false;
 
   formErrors = {
-    'firstname': '',
-    'lastname': '',
-    'telnum': '',
-    'email': ''
+    firstname: '',
+    lastname: '',
+    telnum: '',
+    email: ''
   };
 
   validationMessages = {
-    'firstname': {
-      'required':      'First Name is required.',
-      'minlength':     'First Name must be at least 2 characters long.',
-      'maxlength':     'FirstName cannot be more than 25 characters long.'
+    firstname: {
+      required:      'First Name is required.',
+      minlength:     'First Name must be at least 2 characters long.',
+      maxlength:     'FirstName cannot be more than 25 characters long.'
     },
-    'lastname': {
-      'required':      'Last Name is required.',
-      'minlength':     'Last Name must be at least 2 characters long.',
-      'maxlength':     'Last Name cannot be more than 25 characters long.'
+    lastname: {
+      required:      'Last Name is required.',
+      minlength:     'Last Name must be at least 2 characters long.',
+      maxlength:     'Last Name cannot be more than 25 characters long.'
     },
-    'telnum': {
-      'required':      'Tel. number is required.',
-      'pattern':       'Tel. number must contain only numbers.'
+    telnum: {
+      required:      'Tel. number is required.',
+      pattern:       'Tel. number must contain only numbers.'
     },
-    'email': {
-      'required':      'Email is required.',
-      'email':         'Email not in valid format.'
+    email: {
+      required:      'Email is required.',
+      email:         'Email not in valid format.'
     },
   };
 
@@ -92,7 +92,7 @@ export class ContactComponent implements OnInit {
     console.log(this.feedback);
     this.feedbackService.submitFeedback(this.feedbackCopy).subscribe(feedback => {
        this.feedbackCopy = feedback; this.formSubmitting = false; },
-      errmess => { this.feedback = null; this.errMess = <any> errmess; });
+      errmess => { this.feedback = null; this.errMess = (errmess as any); });
     setTimeout(() => {
       this.feedback = null;
       this.hideForm = false;
